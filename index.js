1f2139cc26d6bcfe69f799d5bb001fe2331c5a28
GanShu <console.log111@gmail.com>
2024-03-29 22:54:42 +0200

    commit part3_a_3.4\3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -50,6 +52,40 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
       response.status(404).end()
     }
+})
+
+// delete person by id
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+// generate id and add new item
+const generateId = () => {
+  return parseInt(Math.random() * 10000)
+}
+
+app.post('/api/persons', (request, response) => {
+  
+const body = request.body
+console.log(body)
+
+if (!body.name) {
+  return response.status(400).json({ 
+  error: 'content missing' 
+  })
+}
+
+const person = {
+  id: generateId(),
+  name: body.name,
+  number: body.number
+}
+
+persons = persons.concat(person)
+
+response.json(person)
 })
 
 const PORT = 3001
