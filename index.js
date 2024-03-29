f49a0521435d56d36c4a6777abc18cc272f864f5
GanShu <console.log111@gmail.com>
2024-03-29 20:55:33 +0200

    commit part3_a_3.3

--- index.js
+++ index.js
@@ -34,10 +34,22 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-// get all persons
+// get person info
 app.get('/info', (request, response) => {
     const date = new Date()
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
+// get person by id
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
 })
 
 const PORT = 3001
